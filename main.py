b1ccc25783b7553f510f3a819570ece014fb976a
KARTIK RAJ OFFICIAL <104128091+KartikRajOfficials@users.noreply.github.com>
2023-01-21 09:47:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from instalooter.looters import PostLooter
 
 logging.basicConfig(level=logging.ERROR)
 
-API_TOKEN = os.environ['5575049794:AAF-6vH38C9VnlXIVHfrgBGbhWoiev_Hwhs']
+API_TOKEN = os.environ['5963970181:AAHvX9KJ84tfiRGDARzsDpFm5-DCysusvzM']
 
 HELP_MESSAGE = \
 '''
